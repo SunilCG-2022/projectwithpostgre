708c21009797aeb86931c97fe2c73e44afa4a498
GIRI <sunil.a.giri@capgemini.com>
2022-08-22 10:48:47 +0530

    backend & create component

---
+++ WeSynergizeAdminScreen/src/main/java/com/wesynergize/WeSynergizeAdminScreenApplication.java
@@ -0,0 +1,13 @@
+package com.wesynergize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeSynergizeAdminScreenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeSynergizeAdminScreenApplication.class, args);
+	}
+
+}
---
+++ WeSynergizeAdminScreen/src/test/java/com/wesynergize/WeSynergizeAdminScreenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wesynergize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeSynergizeAdminScreenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
